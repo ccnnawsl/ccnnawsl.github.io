421ad632ec65d5ab3d986ecac6743e1601852dc3
枞枞那年 <18771770700@163.com>
2024-10-03 14:53:52 +0800

    完善posts

---
+++ download/38498128.js
@@ -0,0 +1,284 @@
+const floorDetail = [{
+  floor: "0",
+  username: "而今听雨",
+  time: "2023-11-24 20:19",
+  reply: "",
+  comment: `<p>本帖是目前mygo直播最高楼帖子的楼层索引：</p>
+    <p>[mygo直播贴] 被朋友拉来直播mygo了，模拟追番，一天一集(尽量)不多看(第11/13话)</p>
+    <p>(又称ansy直播帖、soyo女士亲自直播楼、soyo带你看mygo、mygo观影体式直播楼、acha直播楼)</p>
+    <p><strong>以下索引是不包含剧透的，每一话开始位置</strong></p>
+    <p>警告：如果你想获得最好的补番(？)体验，请从只从这里点索引，不要往下翻了！下面我要发亮点位置了！</p>
+    <figure class='table-figure'>
+      <table>
+        <thead>
+          <tr>
+            <th style='text-align:left;'>第1话开始</th>
+            <th style='text-align:center;'><a href='https://bbs.nga.cn/read.php?pid=728046560&amp;opt=128' target='_blank'>第56楼</a></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style='text-align:left;'>第2话开始</td>
+            <td style='text-align:center;'><a href='https://bbs.nga.cn/read.php?pid=728054978&amp;opt=128' target='_blank'>第85楼</a></td>
+          </tr>
+          <tr>
+            <td style='text-align:left;'>第3话开始</td>
+            <td style='text-align:center;'><a href='https://bbs.nga.cn/read.php?pid=728066755&amp;opt=128' target='_blank'>第112楼</a>
+            </td>
+          </tr>
+          <tr>
+            <td style='text-align:left;'>第4话开始</td>
+            <td style='text-align:center;'><a href='https://bbs.nga.cn/read.php?pid=728100988&amp;opt=128' target='_blank'>第194楼</a>
+            </td>
+          </tr>
+          <tr>
+            <td style='text-align:left;'>第5话开始</td>
+            <td style='text-align:center;'><a href='https://bbs.nga.cn/read.php?pid=728113527&amp;opt=128' target='_blank'>第270楼</a>
+            </td>
+          </tr>
+          <tr>
+            <td style='text-align:left;'>第6话开始</td>
+            <td style='text-align:center;'><a href='https://bbs.nga.cn/read.php?pid=728195355&amp;opt=128' target='_blank'>第361楼</a>
+            </td>
+          </tr>
+          <tr>
+            <td style='text-align:left;'>第7话开始</td>
+            <td style='text-align:center;'><a href='https://bbs.nga.cn/read.php?pid=728276245&amp;opt=128' target='_blank'>第459楼</a>
+            </td>
+          </tr>
+          <tr>
+            <td style='text-align:left;'>第8话开始</td>
+            <td style='text-align:center;'><a href='https://bbs.nga.cn/read.php?pid=728318265&amp;opt=128' target='_blank'>第684楼</a>
+            </td>
+          </tr>
+          <tr>
+            <td style='text-align:left;'>第9话开始</td>
+            <td style='text-align:center;'><a href='https://bbs.nga.cn/read.php?pid=728364483&amp;opt=128' target='_blank'>第1053楼</a>
+            </td>
+          </tr>
+          <tr>
+            <td style='text-align:left;'>第10话开始</td>
+            <td style='text-align:center;'><a href='https://bbs.nga.cn/read.php?pid=728415748&amp;opt=128' target='_blank'>第1583楼</a>
+            </td>
+          </tr>
+          <tr>
+            <td style='text-align:left;'>第11话开始</td>
+            <td style='text-align:center;'><a href='https://bbs.nga.cn/read.php?pid=728705794&amp;opt=128' target='_blank'>第2153楼</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </figure>`,
+}, {
+  floor: "1",
+  username: "而今听雨",
+  time: "2023-11-24 20:22",
+  reply: "",
+  comment: `<p>精彩楼层和简述</p>
+    <figure class='table-figure'>
+      <table>
+        <thead>
+          <tr>
+            <th>根据PV和官方的自我介绍推断角色第一印象</th>
+            <th><a href='https://bbs.nga.cn/read.php?pid=728010018&amp;opt=128' target='_blank'>第27楼</a></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>剪切线第一次发力</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728047028&amp;opt=128' target='_blank'>第58楼</a></td>
+          </tr>
+          <tr>
+            <td>朋友酱第一次间接出场</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728053705&amp;opt=128' target='_blank'>第82楼</a></td>
+          </tr>
+          <tr>
+            <td>吐槽op剧透</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728056287&amp;opt=128' target='_blank'>第89楼</a></td>
+          </tr>
+          <tr>
+            <td>发现路人海玲</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728057783&amp;opt=128' target='_blank'>第95楼</a></td>
+          </tr>
+          <tr>
+            <td>希望soyo利用爱音复辟的回旋镖</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728068554&amp;opt=128' target='_blank'>第121楼</a></td>
+          </tr>
+          <tr>
+            <td>开始回顾1-3话</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728078204&amp;opt=128' target='_blank'>第146楼</a></td>
+          </tr>
+          <tr>
+            <td>四姐妹说的最初提出</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728103492&amp;opt=128' target='_blank'>第203楼</a></td>
+          </tr>
+          <tr>
+            <td>soyo挺武帝一辞挺王于爱音</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728106469&amp;opt=128' target='_blank'>第218楼</a></td>
+          </tr>
+          <tr>
+            <td>很早就在共情soyo</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728110644&amp;opt=128' target='_blank'>第249楼</a></td>
+          </tr>
+          <tr>
+            <td>锐评碧天伴走，丝毫不知大的要来了</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728280444&amp;opt=128' target='_blank'>第487楼</a></td>
+          </tr>
+          <tr>
+            <td>被春日影破防</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728282256&amp;opt=128' target='_blank'>第512楼</a></td>
+          </tr>
+          <tr>
+            <td>楼友整活“手牵手互诉衷肠”</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728284161&amp;opt=128' target='_blank'>第555楼</a></td>
+          </tr>
+          <tr>
+            <td>“如果soyo看到这个帖子，肯定会指着楼主说：天啊这根本就是我”</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728288558&amp;opt=128' target='_blank'>第612楼</a></td>
+          </tr>
+          <tr>
+            <td>楼友整理的第7话反应合集</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728289412&amp;opt=128' target='_blank'>第621楼</a></td>
+          </tr>
+          <tr>
+            <td>继续发展四姐妹说</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728323793&amp;opt=128' target='_blank'>第715楼</a></td>
+          </tr>
+          <tr>
+            <td>soyo惊世一跪</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728330401&amp;opt=128' target='_blank'>第809楼</a></td>
+          </tr>
+          <tr>
+            <td>第9话开头彻底共情soyo</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728365532&amp;opt=128' target='_blank'>第1069楼</a></td>
+          </tr>
+          <tr>
+            <td>“立希还好没说出来”</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728373792&amp;opt=128' target='_blank'>第1176楼</a></td>
+          </tr>
+          <tr>
+            <td>被立希给anon扔直球发言破防</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728374797&amp;opt=128' target='_blank'>第1202楼</a></td>
+          </tr>
+          <tr>
+            <td>顶级节目效果：手里只有9话资源</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728377691&amp;opt=128' target='_blank'>第1297楼</a></td>
+          </tr>
+          <tr>
+            <td>指着soyo说“这简直就是我”</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728424906&amp;opt=128' target='_blank'>第1677楼</a></td>
+          </tr>
+          <tr>
+            <td>anon第一次上号发言</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728427091&amp;opt=128' target='_blank'>第1692楼</a></td>
+          </tr>
+          <tr>
+            <td>楼主的过去</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728436091&amp;opt=128' target='_blank'>第1758楼</a></td>
+          </tr>
+          <tr>
+            <td>女性视角看mygo：soyo的头发和背带</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728514338&amp;opt=128' target='_blank'>第1871楼</a></td>
+          </tr>
+          <tr>
+            <td>soyo0争1</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728517375&amp;opt=128' target='_blank'>第1884楼</a></td>
+          </tr>
+          <tr>
+            <td>直播楼的二创图</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728518899&amp;opt=128' target='_blank'>第1888楼</a></td>
+          </tr>
+          <tr>
+            <td>发糖(？)</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728701209&amp;opt=128' target='_blank'>第2081楼</a></td>
+          </tr>
+          <tr>
+            <td>全网最新的soyo(本人)理解</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728716523&amp;opt=128' target='_blank'>第2341楼</a></td>
+          </tr>
+          <tr>
+            <td>soyo挺武帝二辞挺王于立希</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728720325&amp;opt=128' target='_blank'>第2422楼</a></td>
+          </tr>
+          <tr>
+            <td>女性视角看mygo：锐评爱音的服装审美</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728718134&amp;opt=128' target='_blank'>第2379楼</a></td>
+          </tr>
+          <tr>
+            <td>被11话完全破防</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728727307&amp;opt=128' target='_blank'>第2593楼</a></td>
+          </tr>
+          <tr>
+            <td>长颈鹿集会(读评论)</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728729754&amp;opt=128' target='_blank'>第2639楼</a></td>
+          </tr>
+          <tr>
+            <td>温柔朋友酱上线</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728869556&amp;opt=128' target='_blank'>第2843楼</a></td>
+          </tr>
+          <tr>
+            <td>朋友酱暴露同居</td>
+            <td><a href='https://bbs.nga.cn/read.php?pid=728870190&amp;opt=128' target='_blank'>第2846楼</a></td>
+          </tr>
+        </tbody>
+      </table>
+    </figure>
+    <p>欢迎补充！</p>`,
+}, {
+  floor: "2",
+  username: "彩色的月",
+  time: "2023-11-24 20:34",
+  reply: "",
+  comment: `<p>怎么连直播楼导引都有了……我见识少，这很正常吗？</p>`,
+}, {
+  floor: "4",
+  username: "而今听雨",
+  time: "2023-11-24 20:38",
+  reply: "",
+  comment: `<p><img src="/image/4.jpg" style="zoom: 80%;" /> </p>
+    <p>一个有趣的数据分析：每一话楼层数的变化(注：12话还没播，统计截至今晚8点)</p>`,
+}, {
+  floor: "5",
+  username: "瓜山熏",
+  time: "2023-11-24 20:54",
+  reply: "",
+  comment: `<p>这个楼最草生的还是十集前后，从一个“很不错的直播贴”变成了“十分戏剧性又十分有趣的直播贴”，楼主对待爱音第十话之前是可怜心疼到了第十集后面变得和soyo差不多一个态度起来，太好玩啦</p>`,
+}, {
+  floor: "8",
+  username: "而今听雨",
+  time: "2023-11-24 21:00",
+  reply: "",
+  comment: `<p>后排名词解释：</p>
+    <p>soyo/soyorin：对楼主的称呼。由于楼主观剧全程跟soyo共情程度非常之高，后期更是发表了“soyo第一人称视角”的剧情解析，被大家戏称为soyo本人。soyo女士补番楼由此得名。</p>
+    <p>chaurin：对楼主的称呼。楼主11话补番时，认为soyorin这一昵称特别腻味(就是本人！)，楼友马上将楼主id kngchau改写为chaurin。</p>
+    <p>anon酱：对楼主朋友的称呼。10话结束楼里说楼主简直就是soyo时，有楼友马上表示那楼主损友岂不就是anon。于此得名。</p>
+    <p>ansy楼：后面由于楼主的身体原因、情绪原因、 用电脑太菜的原因 ，楼主朋友经常上号发言，两人互动十分亲密，楼友开始磕两人CP，后来由于戏称两人为soyo anon，索性用ansy专楼指代这一直播楼。</p>`,
+}, {
+  floor: "13",
+  username: "而今听雨",
+  time: "2023-11-24 21:05",
+  reply: "5",
+  comment: `<p>她可不是一直心疼爱音的，刚开播的时候高赞热评就是：“你怎么还没看就开始优化爱音”、“你怎么看了三集还要优化爱音”，以及楼主在第三话看完后发表了“支持soyo利用爱音重组C团”的暴论，
+      <del>前期一直被认为是铁爱音黑</del>
+    </p>`,
+}, {
+  floor: "28",
+  username: "Caresland",
+  time: "2023-11-26 22:17",
+  reply: "",
+  comment: `<p>chaurin看MyGO三件事：共情，落泪，嘴爱音</p>`,
+}, {
+  floor: "35",
+  username: "半世丶浮华。",
+  time: "2023-11-27 19:11",
+  reply: "",
+  comment: `<p>小标几个我个人觉得需要标记的点
+      nga用得少不会标楼层我就直接写楼层数麻烦各位自行跳转啦
+      2928是第12话开始
+      3061是被迷路日击破
+      (3062是看见几只鱼发的迷路日封面二破 笑 )
+      (3435是是我个人在此楼最大的收获，层主总结的很好，原来像chaurin这种心思细腻的妹子看见小睦头那句话反应真的会很大)
+      3436是看完姐妹论的反应(虽然好像只有这一层)</p>`,
+},]
+
+export default floorDetail
